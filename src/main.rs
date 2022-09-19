54c37c3f1d2f2dc58ede39a70d800ca8e0b9febb
Arthur Ming <guobin.ming@pwc.com>
2022-09-19 14:26:27 +0800

    feat: add response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::Read};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -15,5 +15,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Reust: {}", String::from_utf8_lossy(&buffer[..]));
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
