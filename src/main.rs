e3292528c5840fd3c195a489476b7ba74db3ed6e
Arthur Ming <guobin.ming@pwc.com>
2022-09-19 13:56:16 +0800

    feat: hannle request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{net::{TcpListener, TcpStream}, io::Read};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +6,14 @@ fn main() {
     for stream in listener.incoming() {
         let stream =  stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Reust: {}", String::from_utf8_lossy(&buffer[..]));
 }
